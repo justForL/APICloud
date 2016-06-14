9af1d0d3a6b95cf0f15768790bae6df8b5961f81
justForL <layer_james@163.com>
2016-06-14 21:52:54 +0800

    Initial Commit

---
+++ html5/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  html5
+//
+//  Created by James on 16/6/14.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
